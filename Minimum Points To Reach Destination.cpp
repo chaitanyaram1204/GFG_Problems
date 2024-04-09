aa1447a8747b2e1b63c4fb7b0c1a123fc6462965
chaitanyaram1204 <chaitanyaamajala@gmail.com>
2024-04-09 23:17:13 +0530

    09-04-2023 potd

---
+++ Minimum Points To Reach Destination.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+
+public:
+    int find(vector<vector<int>> &points, int i, int j, int m, int n, vector<vector<int>> &dp)
+    {
+        if (i >= m || j >= n || i < 0 || j < 0)
+            return INT_MAX;
+        if (i == m - 1 && j == n - 1)
+            return points[i][j];
+        if (dp[i][j] != INT_MIN)
+            return dp[i][j];
+        int down = points[i][j] + find(points, i + 1, j, m, n, dp);
+        int right = points[i][j] + find(points, i, j + 1, m, n, dp);
+        down = min(down, 0);
+        right = min(right, 0);
+        return dp[i][j] = max(down, right);
+    }
+    int minPoints(int m, int n, vector<vector<int>> points)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, INT_MIN));
+        int val = find(points, 0, 0, m, n, dp);
+        return abs(val) + 1;
+    }
+};
