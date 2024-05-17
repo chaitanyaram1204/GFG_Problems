3556258af0a37d5bacdd30ebabefbdd9b81901f4
chaitanyaram1204 <chaitanyaamajala@gmail.com>
2024-05-17 18:43:38 +0530

    17-05-2024 potd

---
+++ Find Pair Given Difference.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int findPair(int n, int x, vector<int> &arr)
+    {
+        sort(arr.begin(), arr.end());
+
+        int i = 0, j = 1;
+        while (j < n)
+        {
+            int diff = arr[j] - arr[i];
+            if (diff == x)
+                return 1;
+            else if (diff < x)
+                j++;
+            else
+            {
+                i++;
+                if (i == j)
+                    j++;
+            }
+        }
+        return -1;
+    }
+};
