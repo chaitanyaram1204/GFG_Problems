156a63d28f6d62d0dac9519db924ad6fadafccb1
Chaitanya Ram <chaitanyaamajala@gmail.com>
2024-08-03 18:33:50 +0530

    03-08-2024 potd

---
+++ The Celebrity Problem.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int>> &mat)
+    {
+        int n = mat.size();
+        int candidate = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (mat[candidate][i] == 1)
+            {
+                candidate = i;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i != candidate)
+            {
+                if (mat[candidate][i] == 1 || mat[i][candidate] == 0)
+                {
+                    return -1;
+                }
+            }
+        }
+
+        return candidate;
+    }
+};
