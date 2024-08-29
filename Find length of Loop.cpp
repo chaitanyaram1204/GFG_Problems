56b6bf319d236c559706d1640307c37b607be32d
Chaitanya Ram <chaitanyaamajala@gmail.com>
2024-08-29 17:31:41 +0530

    29-08-2024 potd

---
+++ Find length of Loop.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    // Function to find the length of a loop in the linked list.
+    int countNodesinLoop(Node *head)
+    {
+        Node *slow = head, *fast = head;
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+            {
+                int loopLength = 1;
+                Node *temp = slow;
+                while (temp->next != slow)
+                {
+                    loopLength++;
+                    temp = temp->next;
+                }
+                return loopLength;
+            }
+        }
+        return 0;
+    }
+};
