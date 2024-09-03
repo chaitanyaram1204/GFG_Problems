61153b27b0366e1a7a71cb9046eb6c7ce3780b4b
Chaitanya Ram <chaitanyaamajala@gmail.com>
2024-09-03 14:41:04 +0530

    03-09-2024 potd

---
+++ Minimum number of deletions and insertions.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+
+public:
+    int LCS(string &s1, string &s2)
+    {
+        vector<vector<int>> dp(s1.size() + 1, vector<int>(s2.size() + 1, 0));
+        for (int i = 1; i <= s1.size(); i++)
+        {
+            for (int j = 1; j <= s2.size(); j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[s1.size()][s2.size()];
+    }
+
+    int minOperations(string str1, string str2)
+    {
+        int lcs = LCS(str1, str2);
+        // cout << lcs << endl;
+        return str1.size() + str2.size() - (2 * lcs);
+    }
+};
