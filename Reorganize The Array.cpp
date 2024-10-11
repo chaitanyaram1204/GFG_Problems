dcb0e08413fe0ed07fc3df6078a10f51a279c3b9
Chaitanya Ram <chaitanyaamajala@gmail.com>
2024-10-11 07:59:18 +0530

    11-10-2024

---
+++ Reorganize The Array.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> rearrange(const vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> ans(n, -1);
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] != -1)
+            {
+                ans[arr[i]] = arr[i];
+            }
+        }
+
+        return ans;
+    }
+};
