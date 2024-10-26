c9c90c526b61b102ece72bfcd7e0cf1df46cb97d
Chaitanya Ram <chaitanyaamajala@gmail.com>
2024-10-26 12:02:33 +0530

    26-10-2024 potd

---
+++ Occurence of an integer in a Linked List.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int count(struct Node *head, int key)
+    {
+        int count = 0;
+        while (head)
+        {
+            if (head->data == key)
+            {
+                count++;
+            }
+            head = head->next;
+        }
+        return count;
+    }
+};
